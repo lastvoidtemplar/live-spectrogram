f44fa6894ed63fb3d8344050d1366825de0c9d03
lastvoidtemplar <deyandelchev03@abv.bg>
2025-09-21 22:16:02 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import { initWasm } from './lib/wasm-loader';
+import Header from "./layout/header";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./components/ui/resizable";
 
 function App() {
-const [message, setMessage] = React.useState("Loading...");
-
-  React.useEffect(() => {
-    (async () => {
-      const greet = await initWasm();
-      setMessage(greet("React Developer"));
-    })();
-  }, []);
-
   return (
-    <div className="p-6">
-      <h1 className="text-xl font-bold">{message}</h1>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="container mx-auto py-6 flex-1 flex flex-col justify-center">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="flex-1 rounded-lg border"
+        >
+          <ResizablePanel defaultSize={25}>
+            <ResizablePanelGroup direction="vertical">
+              <ResizablePanel defaultSize={25}>
+                <div className="flex h-full items-center justify-center">
+                  <span className="font-semibold">Mic</span>
+                </div>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel defaultSize={75}>
+                <div className="flex h-full items-center justify-center">
+                  <span className="font-semibold">Controls</span>
+                </div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel defaultSize={75}>
+            <div className="flex h-full items-center justify-center">
+              <span className="font-semibold">Canvas</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,5 +1,7 @@
 function Header(){
-    
+    return  <header className="pt-6 pb-2 top-0 text-4xl text-center">
+        Live Spectrogram
+    </header>
 }
 
 export default Header
